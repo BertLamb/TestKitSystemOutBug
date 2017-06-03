ead6874de0f371ed85b9f0cb1093b314fc232628
Bert Lamb <alamb@pobox.com>
2017-06-02 21:50:23 -0400

    Add project files.

---
+++ TestKitSystemOut/NUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Akka.Actor;
+using Akka.TestKit.NUnit3;
+using NUnit.Framework;
+
+namespace TestKitSystemOut.Nunit3
+{
+    [TestFixture]
+    public class NUnitTest : TestKit
+    {
+        private static Props ActorProps => Props.Create(() => new SystemOutActor());
+
+        [Test]
+        public void WriteToConsoleWithActorOf()
+        {
+            Console.Out.WriteLine("This will show up");
+            var actor = ActorOf(ActorProps);
+            Console.Out.WriteLine("So will this");
+            actor.Tell("Console writes from the actor however will not be output");
+            actor.Tell("This one won't either");
+            Console.Out.WriteLine("But this line will show up");
+        }
+
+        [Test]
+        public void WriteToConsoleWithActorOfAsTestActorRef()
+        {
+            var actor = ActorOfAsTestActorRef<SystemOutActor>(ActorProps);
+
+            actor.Tell("Hello World!");
+            actor.Tell("These WILL be output to Standard Out like it should be");
+        }
+    }
+
+    public class SystemOutActor : ReceiveActor
+    {
+        public SystemOutActor()
+        {
+            Receive<string>(str => Console.Out.WriteLine(str));
+        }
+    }
+}
---
+++ TestKitSystemOut/VsTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Akka.Actor;
+using Akka.TestKit.VsTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestKitSystemOut.VsTest
+{
+    [TestClass]
+    public class VsTest : TestKit
+    {
+        private static Props ActorProps => Props.Create(() => new SystemOutActor());
+
+        [TestMethod]
+        public void WriteToConsoleWithActorOf()
+        {
+            var actor = ActorOf(ActorProps);
+
+            actor.Tell("Hello World!");
+            actor.Tell("None of this will be output to Standard Out like it should be");
+
+        }
+
+        [TestMethod]
+        public void WriteToConsoleWithActorOfAsTestActorRef()
+        {
+            var actor = ActorOfAsTestActorRef<SystemOutActor>(ActorProps);
+
+            actor.Tell("Hello World!");
+            actor.Tell("But this WILL be output to Standard Out like it should be");
+        }
+    }
+
+    public class SystemOutActor : ReceiveActor
+    {
+        public SystemOutActor()
+        {
+            Receive<string>(str => Console.Out.WriteLine(str));
+        }
+    }
+}
